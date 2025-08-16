35d42cb37fb8f5fac4eae53bb2b5770d005f615b
Youssef Zaoujal <zaoujalyoussef3@gmail.com>
2025-08-16 23:16:45 +0200

    Fix node_modules permissions

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,7 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   build: {
-    outDir: 'dist', // Spécifie explicitement le dossier de sortie
-    chunkSizeWarningLimit: 1600, // Augmente la limite pour les gros chunks
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Optimisation du découpage des bundles
-          react: ['react', 'react-dom'],
-          lucide: ['lucide-react'],
-        },
-      },
-    },
-  },
-  server: {
-    port: 3000, // Port par défaut pour le dev
-  },
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 });
