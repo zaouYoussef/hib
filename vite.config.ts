7a67174d8b5c91cdfac9f0880f85c0fbcc9b78ad
Youssef Zaoujal <zaoujalyoussef3@gmail.com>
2025-08-16 23:27:52 +0200

    Fix node_modules permissions

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
-  base: '/', // Explicit base URL
-  server: {
-    historyApiFallback: true
-  }
 });
